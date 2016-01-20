aa3ef2f9bfd2728bc446542b72f354db41431080
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2016-01-20 19:19:44 -0300

    Adicionando o gulp, grunt, bower e yeoman

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,22 @@
-if test ! $(which spoof)
+# Correcting npm permissions
+sudo chown -R $(whoami) /usr/local/
+
+if test ! $(which gulp)
 then
-  sudo npm install spoof -g
+  npm install gulp -g
+fi
+
+if test ! $(which grunt)
+then
+  npm install grunt -g
+fi
+
+if test ! $(which bower)
+then
+  npm install bower -g
+fi
+
+if test ! $(which yo)
+then
+  npm install yo -g
 fi
