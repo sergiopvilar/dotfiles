0db80dafb46e2e828651bd4fd0603067e1d274db
Sergio Vilar <sergiovilar.r@gmail.com>
2019-08-15 19:34:06 -0300

    Test if binary is there before installing

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,6 +1,8 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+if test ! $(which rvm)
+then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+fi
 
 nvm install node
