e0da74c44f6715fdae8e78b621a884d5734b6633
Sergio Vilar <sergiovilar.r@gmail.com>
2019-08-15 19:34:16 -0300

    Fix weather alias

--- dev/aliases.zsh
+++ dev/aliases.zsh
@@ -5,7 +5,7 @@ alias run="cd $DEVPROJECT && script/develop"
 alias setup="cd $DEVPROJECT && SSHUSER=$DEVUSER script/setup"
 alias kill_idle="ps aux | grep postgres | grep idle | awk '{print $2}' | xargs kill"
 alias su_setup="cd $DEVPROJECT && SSHUSER=$DEVUSER script/setup -r --ssh-user=$DEVUSER --superuser=$DEVUSER"
-alias tempo='curl -4 http://wttr.in/Jo%C3%A3o%20Pessoa?lang=pt'
+alias tempo='curl -4 "http://wttr.in/Jo%C3%A3o%20Pessoa?lang=pt"'
 
 clean_postgres() {
   sudo rm /usr/local/Evar/postgres/postmaster.pid
