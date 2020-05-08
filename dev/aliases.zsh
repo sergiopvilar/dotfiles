4d5ee36af883c2cb9982d9e80663d259def6b804
Sergio Vilar <sergiovilar.r@gmail.com>
2020-05-08 15:07:38 -0300

    Organize/improve dev aliases

--- dev/aliases.zsh
+++ dev/aliases.zsh
@@ -1,16 +1,23 @@
 alias tower='gittower ./'
 
-# Other Alias
-alias run="cd $DEVPROJECT && script/develop"
-alias setup="cd $DEVPROJECT && SSHUSER=$DEVUSER script/setup"
-alias kill_idle="ps aux | grep postgres | grep idle | awk '{print $2}' | xargs kill"
-alias su_setup="cd $DEVPROJECT && SSHUSER=$DEVUSER script/setup -r --ssh-user=$DEVUSER --superuser=$DEVUSER"
-alias tempo='curl -4 "http://wttr.in/Jo%C3%A3o%20Pessoa?lang=pt"'
+# Project utils
+alias run="cd $DEVPROJECT;script/develop"
+alias setup_sample="cd $DEVPROJECT;SSHUSER=$DEVUSER script/setup --sample"
+alias su_setup="cd $DEVPROJECT;SSHUSER=$DEVUSER script/setup -r --ssh-user=$DEVUSER; bundle exec rails runner $HOME/.dotfiles/dev/su.rb"
+alias reset_vpn="ssh $DEVREMOTE -p 80"
+alias rpush="cd $DEVPROJECT;review push"
 
-clean_postgres() {
+# Php utils
+alias php_server='php -S localhost:3000'
+
+# Postgres Utils
+alias postgres_kill="ps aux | grep postgres | grep idle | awk '{print $2}' | xargs kill"
+postgres_clean() {
   sudo rm /usr/local/Evar/postgres/postmaster.pid
+  sudo rm /usr/local/var/postgres/postmaster.pid
 }
 
+# Docker Utils
 docker_cleanup() {
   docker stop $(docker ps -a -q)
   docker rm $(docker ps -a -q)
