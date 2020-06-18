431ae90895542dd54081c81579fc2e9e596f7b45
Sérgio Vilar <sergiovilar.r@gmail.com>
2020-06-18 19:06:21 -0300

    Ajustes pro FNM funcionar no WSL

--- dev/install.sh
+++ dev/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Linux" ]; then
-  sudo apt-get install golang-go postgresql -y
+  sudo apt-get install golang-go unzip postgresql -y
   sudo /etc/init.d/postgresql restart
   sudo -u postgres createuser -s $(whoami); createdb $(whoami); exit 0
   go get -u github.com/justjanne/powerline-go
