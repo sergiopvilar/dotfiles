bee988174e4b0c6a6bdae7fdbcb468f558ecf13e
Sérgio Vilar <sergio@MacBook-Pro-de-Sergio.local>
2022-03-24 21:32:26 -0300

    Updates do mac os

--- study/install.sh
+++ study/install.sh
@@ -8,30 +8,26 @@ then
   then
     wget -qO- "https://yihui.org/gh/tinytex/tools/install-unx.sh" | sh
   fi
-fi
 
-latex_dependencies="footnotebackref adjustbox babel-german background bidi collectbox csquotes everypage filehook \
-                    footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro \"
-                    sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref babel-portuges setspace koma-script \
-                    colortbl listings caption fancyhdr bookmark"
+  latex_dependencies="footnotebackref adjustbox babel-german background bidi collectbox csquotes everypage filehook \
+                      footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro \"
+                      sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref babel-portuges setspace koma-script \
+                      colortbl listings caption fancyhdr bookmark"
 
-if test "$(uname)" = "Linux"
-then
   tlmgr install $latex_dependencies
-elif test "$(uname)" = "Darwin"
-then
-  sudo tlmgr install $latex_dependencies
+
+  mkdir ~/.pandoc
+  mkdir ~/.pandoc/templates
+
+  if [ -d "$HOME/.pandoc/templates" ]; then
+    echo "Pandoc already installed"
+  else
+    git clone git@github.com:Wandmalfarbe/pandoc-latex-template.git
+    mv pandoc-latex-template/eisvogel.tex ~/.pandoc/templates/eisvogel.latex
+
+    rm -rf pandoc-latex-template
+  fi
+
 fi
 
-mkdir ~/.pandoc
-mkdir ~/.pandoc/templates
-
-if [ -d "$HOME/.pandoc/templates" ]; then
-  echo "Pandoc already installed"
-else
-  git clone git@github.com:Wandmalfarbe/pandoc-latex-template.git
-  mv pandoc-latex-template/eisvogel.tex ~/.pandoc/templates/eisvogel.latex
-
-  rm -rf pandoc-latex-template
-fi
-
+exit 0
